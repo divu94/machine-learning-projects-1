7fb06a58feadfd5982c56d3aec8a8a190dd1122e
divu94 <divyanshusoni95@gmail.com>
2026-01-31 02:22:02 +0530

    changes added

---
+++ src/ml_project_1/utils.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+import sys
+from src.ml_project_1.exception import CustomException
+from src.ml_project_1.logger import logging
+from dotenv import load_dotenv
+import pymysql
+
+load_dotenv()
+
+host = os.getenv("host")
+user = os.getenv("user")
+password = os.getenv("password")
+db = os.getenv("db")
+
+def read_sql_data():
+    logging.info("Reading SQL database started")
+    try:
+        mydb = pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            db=db
+        )
+        logging.info("Connection established",mydb)
+        df = pd.read_sql_query("Select * from students", mydb)
+        print(df.head())
+
+        return df
+    except Exception as ex:
+        raise CustomException(ex)
